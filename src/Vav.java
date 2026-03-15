655660878dc6c036ddbb82981ae982d94b03699a
Александр веденёв <vav396vav@gmail.com>
2026-03-15 11:29:43 +0300

    тренировка

--- src/Vav.java
+++ src/Vav.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class vav {
+public class Vav {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
         list.add(1);
